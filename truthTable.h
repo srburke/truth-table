e2819c33c2f32a7b2a17ae18ead698e1e943496c
Shannon Burke <shayray601@gmail.com>
2021-04-27 20:53:54 -0500

    Truthtable succesfully generates for any given logical expression

--- truthTable.h
+++ truthTable.h
@@ -17,37 +17,50 @@
 #include <iomanip>
 #include <algorithm>
 using namespace std;
-class Expression{
+class Expression
+{
 
-    static int getPrec(char c) {
-		if (c == '~'){
-            return 4;
-        }		
-		else if (c == '^'){
-            return 3;
-        } 
-        else if (c == '+') {
-            return 2;
-        } 
-        else if (c == '>'){
-            return 1;
-        } 
-        else {
-            return 0;
-        }		
+	static int getPrec(char c)
+	{
+		if (c == '~')
+		{
+			return 4;
+		}
+		else if (c == '^')
+		{
+			return 3;
+		}
+		else if (c == '+')
+		{
+			return 2;
+		}
+		else if (c == '>')
+		{
+			return 1;
+		}
+		else
+		{
+			return 0;
+		}
 	}
 
-	static bool isOp(char c) {
+	static bool isOp(char c)
+	{
 		return c == '^' || c == '+' || c == '>' || c == '-' || c == '~';
 	}
 
-	static bool isOperand(char c) {
+	static bool isOperand(char c)
+	{
 		return c >= 'A' && c <= 'Z';
 	}
-      
-public:
 
-    /**
+	static bool isOperandTwo(char c)
+	{
+		return c >= 'a' && c <= 'z';
+	}
+
+public:
+	/**
      * 1) If a character is an operand push that into stack
      * 2) If a character is an operator pop two values from the stack make them its child
      *      and push the current node again
@@ -56,46 +69,57 @@ public:
      * @param infix 
      * @return string 
      */
-    static string toPostFix(string infix){
-        stack<char> opStack;
+	static string toPostFix(string infix)
+	{
+		stack<char> opStack;
 
 		string postfix; //stores final postfix expression
-		char c; //stores each token of infix expression
+		char c;			//stores each token of infix expression
 
 		infix.insert(0, 1, '(');
 		infix.push_back(')');
 
 		postfix = "";
-		for (int i = 0; i < infix.length(); i++) {
+		for (int i = 0; i < infix.length(); i++)
+		{
 			c = infix[i];
-			if (c == '(') {
+			if (c == '(')
+			{
 				opStack.push(c);
 			}
-			else if (isOperand(c)) {
+			else if (isOperand(c))
+			{
 				postfix.push_back(c);
 			}
-			else if (isOp(c)) {
-				while (isOp(opStack.top()) && (getPrec(opStack.top()) >= getPrec(c))) {
+			else if (isOperandTwo(c))
+			{
+				postfix.push_back(c);
+			}
+			else if (isOp(c))
+			{
+				while (isOp(opStack.top()) && (getPrec(opStack.top()) >= getPrec(c)))
+				{
 					char curr = opStack.top();
 					opStack.pop();
 					postfix.push_back(curr);
 				}
 				opStack.push(c);
 			}
-			else if (c == ')') {
-				while (opStack.top() != '(') {
+			else if (c == ')')
+			{
+				while (opStack.top() != '(')
+				{
 					char curr = opStack.top();
 					opStack.pop();
 					postfix.push_back(curr);
-				} 
+				}
 				opStack.pop();
 			}
 		}
 		return postfix;
-    }
+	}
 
-
-/**
+	/**
  * Algorithm to evaluate postfix expressions:
  *  1) Create a stack to store operands (or values).
  *  2) Scan the given expression and do following for every scanned element.
@@ -105,40 +129,51 @@ public:
  *  3) When the expression is ended, the number in the stack is the final answer
  * 
  */
-    static int evalExpr(string& postfix, vector<int>& val){
-        stack<int> exprStack;
+	static int evalExpr(string &postfix, vector<int> &val)
+	{
+		stack<int> exprStack;
 		char c;
 
-		for (int i = 0; i < postfix.length(); i++) {
+		for (int i = 0; i < postfix.length(); i++)
+		{
 			c = postfix[i];
-			if (isOperand(c)) {
+			if (isOperand(c))
+			{
 				exprStack.push(val[c - 'A']);
 			}
-			else if (isOp(c)) {
+			else if (isOperandTwo(c))
+			{
+				exprStack.push(val[c - 'a']);
+			}
+			else if (isOp(c))
+			{
 				int e1, e2;
-				if (c == '~') {
+				if (c == '~')
+				{
 					e1 = exprStack.top();
 					exprStack.pop();
 					exprStack.push(!e1);
 				}
-				else {
+				else
+				{
 					e1 = exprStack.top();
 					exprStack.pop();
 					e2 = exprStack.top();
 					exprStack.pop();
-					switch (c) {
+					switch (c)
+					{
 					case '+':
 						exprStack.push(e2 | e1);
 						break;
 					case '^':
 						exprStack.push(e2 & e1);
 						break;
-                    case '>':
-                        exprStack.push((e2 & !e1) ? 0 : 1);
-                        break;
-                    case '-':
-                        exprStack.push((e2 & e1) || (!e2 & !e1));
-                        break;
+					case '>':
+						exprStack.push((e2 & !e1) ? 0 : 1);
+						break;
+					case '-':
+						exprStack.push((e2 & e1) || (!e2 & !e1));
+						break;
 					}
 				}
 			}
--- truthTableImpl.cpp
+++ truthTableImpl.cpp
@@ -10,95 +10,155 @@
  */
 #include "truthTable.h"
 
-void toBinary(vector<char>& var, vector<int>& val){
+void toBinary(vector<char> &var, vector<int> &val)
+{
 
-    int incr = 1;
-	for (int i = var.size() - 1; i >= 0; i--) {
-		if (val[var[i] - 'A'] == 1 && incr == 1) {
+	int incr = 1;
+	int cr = 1;
+	for (int i = var.size() - 1; i >= 0; i--)
+	{
+		if (val[var[i] - 'A'] == 1 && incr == 1)
+		{
 			val[var[i] - 'A'] = 0;
-			
 		}
-		else if (incr == 1) {
+		else if (incr == 1)
+		{
 			val[var[i] - 'A'] = 1;
 			incr = 0;
 		}
 	}
+
+	for (int j = var.size() - 1; j >= 0; j--)
+	{
+		if (val[var[j] - 'a'] == 1 && cr == 1)
+		{
+			val[var[j] - 'a'] = 0;
+		}
+		else if (cr == 1)
+		{
+			val[var[j] - 'a'] = 1;
+			cr = 0;
+		}
+	}
 }
 
-void printTable(string s){
-    vector<char> var; //stores all variables that are present in the boolean expression s
-	vector<int> val(26, -1); //stores the value of each boolean variable
-							
-	string postfix; 
+void printTable(string s)
+{
+	vector<char> var;		 //stores all variables that are present in the boolean expression s
+	vector<int> val(52, -1); //stores the value of each boolean variable
 
-	for (int i = 0; i < s.length(); i++) {
+	string postfix;
 
-		if (s[i] >= 'A' && s[i] <= 'Z' && val[s[i] - 'A'] == -1) {
+	for (int i = 0; i < s.length(); i++)
+	{
+
+		if (s[i] >= 'A' && s[i] <= 'Z' && val[s[i] - 'A'] == -1)
+		{
 			val[s[i] - 'A'] = 0;
 			var.push_back(s[i]);
 		}
-
+		else if (s[i] >= 'a' && s[i] <= 'z' && val[s[i] - 'a'] == -1)
+		{
+			val[s[i] - 'a'] = 0;
+			var.push_back(s[i]);
+		}
 	}
 
 	postfix = Expression::toPostFix(s);
 
-	for (int i = 0; i < var.size(); i++) {
-		cout << "|" << var[i] << setw(1);
+	char c;
+
+	for (int i = 0; i < var.size(); i++)
+	{
+		if (var[i] >= 'A' && var[i] <= 'Z' == 1)
+		{
+			cout << "|" << var[i] << setw(1);
+		}
+		else if (var[i] >= 'a' && var[i] <= 'z' == 1)
+		{
+			cout << "|" << var[i] << setw(1);
+		}
 	}
 	cout << "|" << s << setw(1) << endl;
 
-	for(int i = 0; i < var.size() + 1; i++){
+	for (int i = 0; i < var.size() + 1; i++)
+	{
 		cout << "+";
-		for(int j = 0; j < 1; j++){
+		for (int j = 0; j < 1; j++)
+		{
 			cout << "-";
 		}
 	}
 	cout << "\n";
 
-	for (int i = 0; i < pow(2, var.size()); i++) {
+	for (int i = 0; i < pow(2, var.size()); i++)
+	{
 
-		for (int i = 0; i < var.size(); i++) {
-			if(val[var[i] - 'A'] == 0){
-				cout << "|" << "F" << setw(1);
+		for (int i = 0; i < var.size(); i++)
+		{
+			if (val[var[i] - 'A'] == 0)
+			{
+				cout << "|"
+					 << "F" << setw(1);
 			}
-			if(val[var[i] - 'A'] == 1){
-				cout << "|" << "T" << setw(1);
+			else if (val[var[i] - 'A'] == 1)
+			{
+				cout << "|"
+					 << "T" << setw(1);
+			}
+
+			else if (val[var[i] - 'a'] == 0)
+			{
+				cout << "|"
+					 << "F" << setw(1);
+			}
+
+			else if (val[var[i] - 'a'] == 1)
+			{
+				cout << "|"
+					 << "T" << setw(1);
 			}
 		}
 
-		if(Expression::evalExpr(postfix, val) == 0){
-			cout << "|" << "F" << endl;
+		if (Expression::evalExpr(postfix, val) == 0)
+		{
+			cout << "|"
+				 << "F" << endl;
 		}
-		if(Expression::evalExpr(postfix, val) == 1){
-			cout << "|" << "T" << endl;
+		if (Expression::evalExpr(postfix, val) == 1)
+		{
+			cout << "|"
+				 << "T" << endl;
 		}
 		toBinary(var, val);
 
-		for(int k = 0; k < var.size() + 1; k++){
+		for (int k = 0; k < var.size() + 1; k++)
+		{
 			cout << "+";
-			for(int z = 0; z < 1; z++){
-                cout << "-";
+			for (int z = 0; z < 1; z++)
+			{
+				cout << "-";
 			}
 		}
 		cout << "\n";
 	}
 }
 
-int main(){
+int main()
+{
 
-    cout << "*----------------------------------------------------------------------------------------------------------------*\n";
-    cout << "* This program generates truth tables for propositional logic formulas.                                          *\n";
-    cout << "* You can enter the given logical operators: AND(^), OR(+), NOT(~), COND(>), BICOND(-) in many different formats.*\n";
-    cout << "* For example, you may enter a propositional logic such as: (P^Q)+(Q^C) and also one as: ~(P+Q)^(P>C) and etc.   *\n";
-    cout << "*----------------------------------------------------------------------------------------------------------------*\n";
+	cout << "*----------------------------------------------------------------------------------------------------------------*\n";
+	cout << "* This program generates truth tables for propositional logic formulas.                                          *\n";
+	cout << "* You can enter the given logical operators: AND(^), OR(+), NOT(~), COND(>), BICOND(-) in many different formats.*\n";
+	cout << "* Examples, of propositional logic you may enter: (P^Q)+(Q^C) or maybe ~(p+q)^(p>c) and etc.                     *\n";
+	cout << "* You can use both uppercase and lowercase chars for your expression.   										  *\n";
+	cout << "*----------------------------------------------------------------------------------------------------------------*\n";
 
-    string s;
-    cout << "Enter Boolean Expression: ";
-    cin >> s;
-    cout << "\n";
-    cout << "---------------------------------------------------------\n";
+	string s;
+	cout << "Enter Boolean Expression: ";
+	cin >> s;
+	cout << "\n";
+	cout << "---------------------------------------------------------\n";
 
-    printTable(s);
+	printTable(s);
 }
-
-
